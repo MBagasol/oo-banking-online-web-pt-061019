566bf51cbf5cf617978cc68c629a7832b02a9ef6
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-08-02 01:43:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,8 @@
 class BankAccount
-  attr_accessor :bank_account 
+  attr_accessor :bank_account , :name
   
   def initialize(bank_account)
     @bank_account = bank_account
+    @name = name
   end
 end
